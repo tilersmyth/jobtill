565b3166f2394169bc36431881db08159f7dfa96
greeneggsandspam <buildwithsam@gmail.com>
2015-01-09 15:26:23 -0500

    Updated to reflect last pull, total sites scraped, total job posings

--- js/app.js
+++ js/app.js
@@ -2,8 +2,10 @@ var app = angular.module("Scrape", ["firebase"]);
 
 //Firebase Link
 app.controller("ScrapeCtrl", function($scope, $firebase) {
-  var ref = new Firebase("https://glowing-inferno-8009.firebaseio.com/listings");
-  var sync = $firebase(ref);
-  $scope.sync = sync.$asObject();
+  var ref = new Firebase("https://glowing-inferno-8009.firebaseio.com");
+  var data = $firebase(ref.child('listings'));
+  $scope.data = data.$asObject();
+  var updated = $firebase(ref.child('LastScraped'));
+  $scope.updated = updated.$asObject();
 });
 
