c56098a283d2404dd2d842b5b2d1d65482470661
greeneggsandspam <buildwithsam@gmail.com>
2015-01-18 20:24:26 -0500

    User Data Pullah

--- js/app.js
+++ js/app.js
@@ -24,74 +24,81 @@ app.controller("ScrapeCtrl", function($scope, $firebase) {
 
 });
 
-app.controller("UserCtrl", ["$scope", "$firebaseAuth",
-  function($scope, $firebaseAuth) {
-    var ref2 = new Firebase("https://glowing-inferno-8009.firebaseio.com");
-	var usersRef = ref2.child("users");
-    $scope.authObj = $firebaseAuth(ref2);
-	$scope.authData = $scope.authObj.$getAuth();
+app.controller("UserCtrl", ["$scope", "$firebaseAuth", "$firebase",
+  function ($scope, $firebaseAuth, $firebase) {
+        var ref2 = new Firebase("https://glowing-inferno-8009.firebaseio.com");
+        var usersRef = ref2.child("users");
+        $scope.authObj = $firebaseAuth(ref2);
+        $scope.authData = $scope.authObj.$getAuth();
+        if ($scope.authData !== null) {
+            var userInfo = $firebase(usersRef.child($scope.authData.uid));
+            $scope.userData = userInfo.$asObject();
+        };
+        //clear validation text on dropdown close
+        $scope.toggled = function (open) {
+            if (open === false)
+                $scope.loginmsg = "";
+        };
 
-    //clear validation text on dropdown close
-   $scope.toggled = function(open) {
-    if(open === false)
-    $scope.loginmsg = "";
-  };
+        //Sign up
+        $scope.signUp = function (a, b, c, d) {
+            if (a !== undefined && b !== undefined) {
+                $scope.authObj.$createUser({
+                    email: a,
+                    password: b
+                }).then(function (userData) {
+                    return $scope.authObj.$authWithPassword({
+                        email: a,
+                        password: b
+                    });
+                }).then(function (authData) {
+                    $scope.authData = $scope.authObj.$getAuth();
+                    var userInfo = $firebase(usersRef.child($scope.authData.uid));
+                    $scope.userData = userInfo.$asObject();
+                    var usertoPush = $scope.authObj.$getAuth();
+                    usersRef.child(usertoPush.uid).set({
+                        firstName: c,
+                        lastName: d,
+                        email: usertoPush.password.email,
+                        provider: usertoPush.provider,
+                        joined: new Date().getTime(),
+                        search_tokens: "0",
+                        search_terms: ""
+                    });
+                    $scope.status.isopen = !$scope.status.isopen;
+                }).catch(function (error) {
+                    console.error("Error: ", error);
+                });
+            } else {
+                $scope.signupmsg = "Please enter username and password.";
+            };
+        };
 
-//Sign up
-$scope.signUp = function (a, b, c, d) {
-    if (a !== undefined && b !== undefined) {
-        $scope.authObj.$createUser({
-            email: a,
-            password: b
-        }).then(function (userData) {
-            return $scope.authObj.$authWithPassword({
-                email: a,
-                password: b
-            });
-        }).then(function (authData) {
+        //Log in	
+        $scope.logIn = function (a, b) {
+            if (a !== undefined && b !== undefined) {
+                $scope.authObj.$authWithPassword({
+                    email: a,
+                    password: b
+                }).then(function (authData) {
+                    $scope.authData = $scope.authObj.$getAuth();
+                    var userInfo = $firebase(usersRef.child($scope.authData.uid));
+                    $scope.userData = userInfo.$asObject();
+                    $scope.loginmsg = "Login Success!";
+                    $scope.status.isopen = !$scope.status.isopen;
+                }).catch(function (error) {
+                    $scope.loginmsg = error.message;
+                });
+            } else {
+                $scope.loginmsg = "Please enter username and password.";
+            };
+        };
+        //Log out
+        $scope.logout = function () {
+            $scope.authObj.$unauth();
             $scope.authData = $scope.authObj.$getAuth();
-            var usertoPush = $scope.authObj.$getAuth();
-            usersRef.child(usertoPush.uid).set({
-                firstName: c,
-                lastName: d,
-                email: usertoPush.password.email,
-                provider: usertoPush.provider,
-                joined: new Date().getTime(),
-                search_tokens: "0",
-                search_terms: ""
-            });
-            $scope.status.isopen = !$scope.status.isopen;
-        }).catch(function (error) {
-            console.error("Error: ", error);
-        });
-    } else {
-        $scope.signupmsg = "Please enter username and password.";
-    };
-};      
-
-//Log in	
-$scope.logIn = function (a, b) {
-    if (a !== undefined && b !== undefined) {
-        $scope.authObj.$authWithPassword({
-            email: a,
-            password: b
-        }).then(function (authData) {
-            $scope.authData = $scope.authObj.$getAuth();
-            $scope.loginmsg = "Login Success!";		
-			$scope.status.isopen = !$scope.status.isopen;	
-        }).catch(function (error) {
-            $scope.loginmsg = error.message;
-        });
-    } else {
-        $scope.loginmsg = "Please enter username and password.";
-    };
-};
-	//Log out
-		$scope.logout = function() {
-			$scope.authObj.$unauth();
-			$scope.authData = $scope.authObj.$getAuth();
-		};
-	//Close Controller
+        };
+        //Close Controller
   }
 ]);
 
