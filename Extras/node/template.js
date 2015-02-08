ae6ee6b22a3c80e95c55763746fcde94679a0553
Tyler Smith <tyler.smith.la@gmail.com>
2015-02-08 16:31:27 -0500

    change subtext verbiage

--- Extras/node/template.js
+++ Extras/node/template.js
@@ -2,20 +2,21 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 
-request('http://careers.draftkings.com/', function (error, response, html) {
+request('https://otelic.com/quanttus/jobs', function (error, response, html) {
     if (!error && response.statusCode == 200) {
         var $ = cheerio.load(html);
         var listCount = 0;
-        $(".resumator-job-title a").each(function (i, element) {
+        $(".job-title a").each(function (i, element) {
             listCount++;
 
-            var a = $(this).parent().next().text().trim().split(":")[1];
+           var a = $(this).parent().prev().text().trim();
 		   
  
             var name = $(this).text();
-            var location = "Boston, MA";			
-            var url = $(this).attr('href');
-			
+            var location = $(this).children('.jobTeaser').children('strong').text().split("-")[1]?$(this).children('.jobTeaser').children('strong').text().split("-")[1].trim():"Boston, MA";		
+            var url = $(this).children('.jobBtn').children('a').attr('href');
+
+
 			console.log(a);
         });
         
