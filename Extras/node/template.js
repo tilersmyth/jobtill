7cb7f1e94d63b9fa3348d800e3ed2dcafd336999
greeneggsandspam <buildwithsam@gmail.com>
2015-02-07 20:56:36 -0500

    Nothing

--- Extras/node/template.js
+++ Extras/node/template.js
@@ -2,19 +2,19 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 
-request('http://www.anaqua.com/about-anaqua/careers.html', function (error, response, html) {
+request('http://careers.draftkings.com/', function (error, response, html) {
     if (!error && response.statusCode == 200) {
         var $ = cheerio.load(html);
         var listCount = 0;
-        $("u:contains(\"United\")").parent().siblings("a").each(function (i, element) {
+        $(".resumator-job-title a").each(function (i, element) {
             listCount++;
 
-            var a = $(this).text().slice(2);
+            var a = $(this).parent().next().text().trim().split(":")[1];
 		   
  
-            var name = $(this).text().slice(2);
+            var name = $(this).text();
             var location = "Boston, MA";			
-            var url = "http://www.anaqua.com/about-anaqua/"+ $(this).attr("href");
+            var url = $(this).attr('href');
 			
 			console.log(a);
         });
